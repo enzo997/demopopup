c181d9e0453461a7c95fed07b8cc9388d276ee6b
phamvanday <phamday999@gmail.com>
2020-03-17 08:05:12 +0700

    css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,27 @@ $ = jQuery;
 
 $(document).ready(function(){
     $('.btn-popup').click(function(){
-        $('.popup').toggleClass('show-popup');
+        $('.popup').addClass('show-popup'),$('.black-background').addClass('active');
     });
     $('.close').click(function(){
-        $('.popup').removeClass('show-popup');
-    })
+        $('.popup').removeClass('show-popup'),$('.black-background').removeClass('active');
+    });
+    $('.p1').click(function(){
+        $('.popup-child-1').addClass('show-1');
+    });
+    $('.p2').click(function(){
+        $('.popup-child-2').addClass('show-2');
+    });
+    $('.p3').click(function(){
+        $('.popup-child-3').addClass('show-3');
+    });
+    $('.close-1').click(function(){
+        $('.popup-child-1').removeClass('show-1');
+    });
+    $('.close-2').click(function(){
+        $('.popup-child-2').removeClass('show-2');
+    });
+    $('.close-3').click(function(){
+        $('.popup-child-3').removeClass('show-3');
+    });
 });
